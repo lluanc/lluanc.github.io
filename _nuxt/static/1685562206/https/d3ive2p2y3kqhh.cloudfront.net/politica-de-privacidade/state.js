ed517654e9beb704b3cb9b693de82d4ba5791aef
lluanc <101584692+lluanc@users.noreply.github.com>
2023-05-31 19:59:34 +0000

    vsvvv

--- _nuxt/static/1685562206/https/d3ive2p2y3kqhh.cloudfront.net/politica-de-privacidade/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1685562206",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fhttps:\u002F\u002Fd3ive2p2y3kqhh.cloudfront.net\u002Fpolitica-de-privacidade"));
---
+++ _nuxt/static/1685563013/https/d3ive2p2y3kqhh.cloudfront.net/politica-de-privacidade/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1685563013",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fhttps:\u002F\u002Fd3ive2p2y3kqhh.cloudfront.net\u002Fpolitica-de-privacidade"));
